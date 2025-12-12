e24c449680835df6ef2b812ab7fcfdf13a90341e
Pâmela dos Reis <pammreiss@gmail.com>
2025-12-12 19:21:08 -0300

    feat: Adicionei Create, Update e Find By Id da entidade Plano

--- src/plano/controllers/plano.controller.ts
+++ src/plano/controllers/plano.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -6,6 +7,7 @@ import {
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
 } from '@nestjs/common';
 import { Plano } from '../entities/plano.entity';
 import { PlanoService } from '../services/plano.service';
@@ -18,6 +20,18 @@ export class PlanoController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Plano[]> {
     return this.planoService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Plano> {
+    return this.planoService.findById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() plano: Plano): Promise<Plano> {
+    return this.planoService.create(plano);
   }
 
   @Delete('/:id')
--- src/plano/services/plano.service.ts
+++ src/plano/services/plano.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpCode, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DeleteResult, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Plano } from '../entities/plano.entity';
@@ -17,6 +17,29 @@ export class PlanoService {
       },
     });
   } 
+
+  async findById(id: number): Promise<Plano> {
+
+    const plano = await this.planoRepository.findOne({
+      where: {
+        id
+      }
+    });
+
+    if (!plano)
+      throw new HttpException('Plano não encontrado!', HttpStatus.NOT_FOUND);
+
+    return plano;
+  }
+
+  async create(plano: Plano): Promise<Plano> {
+    return await this.planoRepository.save(plano);
+  }
+
+  async update(plano: Plano): Promise<Plano> {
+    await this.findById(plano.id)
+    return await this.planoRepository.save(plano);
+  }
 
   async delete(id: number): Promise<DeleteResult> {
     await this.findById(id);
