dc09b68bebbb46cdf340db5618ffd8b5ce249799
lf-kali <cbjk.kali@gmail.com>
2025-12-12 15:13:09 -0300

    fix: declaração de classes

---
+++ src/plano/entities/plano.entity.ts
@@ -0,0 +1,6 @@
+import { Entity } from "typeorm";
+
+@Entity("tb_planos")
+export class Plano {
+
+}
--- src/plano/plano.module.ts
+++ src/plano/plano.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Plano } from "./entities/plano.entity";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Plano])]
 })
+export class PlanoModule {}
