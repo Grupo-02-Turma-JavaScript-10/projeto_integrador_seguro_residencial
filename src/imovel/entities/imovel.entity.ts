b34b3949a5ba0d66ef2b5a74be515e2155f3ae28
Eduardo <eduardo.olvreis@gmail.com>
2025-12-12 14:45:41 -0300

    feat: imovel entity

--- src/imovel/entities/imovel.entity.ts
+++ src/imovel/entities/imovel.entity.ts
@@ -1,8 +1,33 @@
-import { Entity } from 'typeorm';
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({ name: 'tb_imoveis' })
+@Entity({ name: "tb_imoveis" })
 export class Imovel {
 
+    @PrimaryGeneratedColumn()
+    id: number
 
-    
+    @IsNotEmpty()
+    @Column({ length: 8, nullable: false })
+    cep: string
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    bairro: string
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    rua: string
+
+    @IsNotEmpty()
+    @Column({ type: "int", nullable: false })
+    numero: number
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    tipoImovel: string
+
+    @IsNotEmpty()
+    @Column({ type: "decimal", precision: 10, scale: 2, nullable: false })
+    areaConstruida: number
 }
